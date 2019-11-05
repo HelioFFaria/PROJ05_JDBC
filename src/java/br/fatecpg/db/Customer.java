43886dacf3d9e3adfc06e532b51e2ed6b289062a
a <a@10.0.2.15>
2019-11-04 22:25:18 -0200

    Fifith commit

---
+++ src/java/br/fatecpg/db/Customer.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.fatecpg.db;
+
+/**
+ *
+ * @author a
+ */
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
