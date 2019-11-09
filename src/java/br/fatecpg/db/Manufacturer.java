433dbf9e39fb64ae418e168465820033e9b0eca3
HelioFFaria <helioffaria76@gmail.com>
2019-11-09 21:12:02 -0200

    Inclusão na Classe Manufacturer os atributos, Método Construtor, Método GET e SET, SELECT no JavaDB dos atributos name, cidade, estado e email.

--- src/java/br/fatecpg/db/Manufacturer.java
+++ src/java/br/fatecpg/db/Manufacturer.java
@@ -17,9 +17,14 @@ import java.util.ArrayList;
  */
 public class Manufacturer {
     private int id;
+    //Incluíndo as variáveis String name, cidade, estado e email, como atributos da classe Manufacturer, 
+    //conforme solicitado no enunciado do projeto_05.
     private String name;
+    private String cidade;
+    private String estado;
     private String email;
-
+    //...
+ 
     public static ArrayList<Manufacturer> getList() throws Exception {
         ArrayList<Manufacturer> list = new ArrayList();
         Class.forName("org.apache.derby.jdbc.ClientDriver");
@@ -29,11 +34,18 @@ public class Manufacturer {
         
         Connection con = DriverManager.getConnection(url, user, pass);
         Statement stmt = con.createStatement();
-        String sql = "SELECT manufacturer_id, name, email FROM MANUFACTURER";
+        //Incluindo SELECT dos atributos NAME, CITY, STATE e EMAIL da Entidade MANUFACTURER,
+        //conforme solicitado no enunciado do projeto_05.
+        String sql = "SELECT manufacturer_id, name, city, state, email FROM MANUFACTURER";
+        //...
         ResultSet rs = stmt.executeQuery(sql);
         
         while(rs.next()) {
-            Manufacturer c = new Manufacturer(rs.getInt("manufacturer_id"), rs.getString("name"), rs.getString("email"));
+            //Instanciando Manufacturer c, adicionando os atributos NAME, CITY, STATE e EMAIL
+            //da Entidade MANUFACTURER, conforme solicitado no enunciado do projeto_05.  
+            Manufacturer c = new Manufacturer(rs.getInt("manufacturer_id"), rs.getString("name"),
+                rs.getString("city"), rs.getString("state"), rs.getString("email"));
+            //...
             list.add(c);
         }
         
@@ -42,35 +54,57 @@ public class Manufacturer {
     
     public Manufacturer() {
     }
-
-    public Manufacturer(int id, String name, String email) {
+    //Passando os parâmetros String name, cidade, estado e email no Método Construtor da classe Manufacturer, 
+    //e atribuindo os mesmos aos atributos da classe, conforme solicitado no enunciado do projeto_05.
+    public Manufacturer(int id, String name, String city, String state, String email) {
         this.id = id;
+        //...
         this.name = name;
+        this.cidade = city;
+        this.estado = state;
         this.email = email;
+        //...
     }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getId() {
+      public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
     }
-
-    public String getName() {
+    //Criando os métodos GET e SET dos atributos name, cidade, estado e email da classe Manufacturer.
+    //conforme solicitado no enunciado do projeto_05.
+    public String getName() { 
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    //...    
     }
 
 }
